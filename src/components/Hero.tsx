bfe43d822f46421a53034b308771f63606f54546
Michele <mikzed@hotmail.it>
2025-08-08 20:48:27 +0200

    overflow x hidden

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div className="relative bg-amber-950 h-100 md:h-80 mt-25 md:mt-14">
       <div className="flex justify-center lg:items-center mx-auto max-w-screen-lg">
-        <div className="flex flex-col my-10 mx-4 lg:basis-1/2 ">
+        <div className="flex flex-col my-10 mx-4 lg:basis-1/2">
           <h1 className="text-6xl text-white">Cook it. Love it. Share it.</h1>
           <div className="flex gap-2 mt-6">
             <a href="/#trending-recipes" className="z-3">
@@ -18,7 +18,7 @@ const Hero = () => {
             alt="frying pan"
             className="absolute top-35 sm:top-30 md:top-5 lg:-top-10 -right-15 xl:right-40 2xl:right-100 lg:w-150 xl:-rotate-15 z-2"
           />
-          <div className="absolute rounded-full h-50 w-50 bg-neutral-800 blur-xl lg:blur-2xl -skew-x-50 rotate-28 top-95 md:top-75 lg:top-75 xl:top-90 right-30 lg:right-40 xl:right-90 2xl:right-150 z-1" />
+          <div className="absolute rounded-full h-50 w-50 bg-neutral-800 blur-xl lg:blur-2xl -skew-x-50 rotate-28 top-85 md:top-75 lg:top-75 xl:top-90 right-20 lg:right-40 xl:right-90 2xl:right-150 z-1" />
         </div>
       </div>
     </div>
